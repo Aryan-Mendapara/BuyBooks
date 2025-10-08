5da7a1d4e4eed8079b702d7ca674ccc99ac665c7
Aryan-Mendapara <aryanmendapara146@gmail.com>
2025-10-08 15:43:15 +0530

    first commit

--- server/src/Routes/BestBooks.js
+++ server/src/Routes/BestBooks.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const uploads = require("../Middleware/multerStorage");
-const { createBooks, getBooks, deleteBooks, searchBooks } = require("../Controller/BooksController");
+const { createBooks, getBooks, deleteBooks } = require("../Controller/BooksController");
 
 router.post('/import', uploads.single('image'), createBooks);
 router.get('/get', getBooks);
-router.get('/search', searchBooks);
 router.delete('/delete/:id', deleteBooks);
 
 module.exports = router
