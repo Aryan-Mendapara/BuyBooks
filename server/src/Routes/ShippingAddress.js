371b35c120ca0f0bc7a4ed3ffdf48caa151696f6
Aryan-Mendapara <aryanmendapara146@gmail.com>
2025-10-08 17:17:07 +0530

    first commit

--- server/src/Routes/ShippingAddress.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { addAddress, getAddress, deleteAddress } = require("../Controller/ShippingAddress");
-const ShippingAddress = express.Router();
-
-ShippingAddress.post("/add", addAddress);
-ShippingAddress.get("/get", getAddress);
-ShippingAddress.delete("/delete/:id", deleteAddress);
-
-module.exports = ShippingAddress;
