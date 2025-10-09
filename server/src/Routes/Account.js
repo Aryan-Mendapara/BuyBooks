5d719cee69f053966126e76da0fdf2f1063fe7cd
Aryan-Mendapara <aryanmendapara146@gmail.com>
2025-10-09 18:46:14 +0530

    first commit

--- server/src/Routes/Account.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { addAccount, deleteAccount } = require("../Controller/Account");
-const accountRouter = express.Router();
-
-accountRouter.post("/add",addAccount);
-accountRouter.delete("/delete/:id", deleteAccount);
-
-module.exports = accountRouter;
--- server/src/Routes/WishList.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { wishlistAdd, wishlistGet, wishlistDelete } = require("../Controller/WishList");
-const wishlist = express.Router();
-
-wishlist.post("/add", wishlistAdd);
-wishlist.get("/get", wishlistGet);
-wishlist.delete("/delete/:id", wishlistDelete);
-
-module.exports = wishlist;
