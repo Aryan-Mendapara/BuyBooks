f0d29abb1a3931062f51383944332fda98383753
Aryan-Mendapara <aryanmendapara146@gmail.com>
2025-11-04 17:42:53 +0530

    Home

--- client/src/Component/Pages/Home.jsx
+++ client/src/Component/Pages/Home.jsx
@@ -1,19 +1,20 @@
 import React, { useContext, useEffect, useState } from 'react';
-import schoolbooks from '../../assets/img/schoolbooks.jpg';
-import bestseller from '../../assets/img/bestseller.jpg';
-import BBIchildrenandadult from '../../assets/img/BBIchildrenandadult.jpg';
-import BBIJournal from '../../assets/img/BBIJournal.jpg';
-import governmentbook from '../../assets/img/governmentbook.jpg';
-import testpep from '../../assets/img/testpep.jpg';
+import schoolbooks from '../../assets/img/school-books.jpg';
+import bestseller from '../../assets/img/best-seller.jpg';
+import BBIchildrenandadult from '../../assets/img/children-and-young-adult.jpg';
+import BBIJournal from '../../assets/img/bbi-journal.jpg';
+import governmentbook from '../../assets/img/goverment-book.jpg';
+import testpep from '../../assets/img/test-pep.jpg';
 import advitbanner from '../../assets/img/advitbanner.jpg';
-import boxsets from '../../assets/img/boxsets.jpg';
+import boxsets from '../../assets/img/box-sets.jpg';
 import BBIDiscountOffer from '../../assets/img/BBIDiscountOffer.jpg';
 import PopularAuthor from '../../assets/img/PopularAuthor.png';
 import BBILanguagesbanner from '../../assets/img/BBILanguagesbanner.jpg'
 import NewArrivals from './NewArrivals/NewArrivals';
 import BestSeller from './BestSellers/BestSellers';
 import SchoolBooks from './SchoolBooks/SchoolBooks';
-import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import { VscChevronLeft, VscChevronRight } from "react-icons/vsc";
+
 import FictionNonFictionBooks from './FictionNonFiction/FictionNonFictionBooks';
 import BooksbyCategories from './BooksbyCategories/BooksbyCategories';
 import { Link } from 'react-router-dom';
@@ -73,27 +74,38 @@ function Home() {
           <button
             onClick={handlePrev}
             disabled={isAnimating}
-            className='absolute top-1/2 left-0 -translate-y-1/2 z-10 bg-black/70 p-5 shadow cursor-pointer'
+            className='absolute top-1/2 left-0 -translate-y-1/2 z-10 cursor-pointer'
           >
-            <FaChevronLeft className='text-xl text-white' />
+            <VscChevronLeft className='text-xl text-blue-600' size={80} />
           </button>
 
           {/* Slide Image */}
-          <Link to={images[currentIndex].path}>
-            <img
-              src={images[currentIndex].src}
-              alt={images[currentIndex].name}
-              className="w-full h-full object-cover transition-all duration-700"
-            />
-          </Link>
+          <div className="w-full h-full relative overflow-hidden">
+            <div
+              className="flex transition-transform duration-800 ease-in-out"
+              style={{
+                transform: `translateX(-${currentIndex * 100}%)`,
+              }}
+            >
+              {images.map((img) => (
+                <Link key={img.id} to={img.path} className="w-full flex-shrink-0">
+                  <img
+                    src={img.src}
+                    alt={img.name}
+                    className="w-full h-full object-cover"
+                  />
+                </Link>
+              ))}
+            </div>
+          </div>
 
           {/* Next Button */}
           <button
             onClick={handleNext}
             disabled={isAnimating}
-            className='absolute right-0 top-1/2 -translate-y-1/2 z-10 bg-black/70 p-5 shadow cursor-pointer'
+            className='absolute right-0 top-1/2 -translate-y-1/2 z-10 cursor-pointer'
           >
-            <FaChevronRight className='text-xl text-white' />
+            <VscChevronRight className='text-xl text-blue-600' size={80} />
           </button>
         </div>
 
@@ -103,27 +115,36 @@ function Home() {
           <button
             onClick={handlePrev}
             disabled={isAnimating}
-            className='absolute top-1/2 left-0 -translate-y-1/2 z-10 bg-black/70 rounded-full p-1 shadow cursor-pointer'
+            className='absolute top-1/2 left-0 -translate-y-1/2 z-10 cursor-pointer'
           >
-            <FaChevronLeft className='text-xl text-white' />
+            <VscChevronLeft className='text-blue-600' size={40} />
           </button>
 
           {/* Slide Image */}
-          <Link to={images[currentIndex].path}>
-            <img
-              src={images[currentIndex].src}
-              alt={images[currentIndex].name}
-              className="w-full h-full object-cover transition-all duration-700"
-            />
-          </Link>
+          <div
+            className="flex transition-transform duration-800 ease-in-out"
+            style={{
+              transform: `translateX(-${currentIndex * 100}%)`,
+            }}
+          >
+            {images.map((img) => (
+              <Link key={img.id} to={img.path} className="w-full flex-shrink-0">
+                <img
+                  src={img.src}
+                  alt={img.name}
+                  className="w-full h-full object-cover"
+                />
+              </Link>
+            ))}
+          </div>
 
           {/* Next Button */}
           <button
             onClick={handleNext}
             disabled={isAnimating}
-            className='absolute right-0 top-1/2 -translate-y-1/2 z-10 bg-black/70 rounded-full p-1 shadow cursor-pointer'
+            className='absolute right-0 top-1/2 -translate-y-1/2 z-10 cursor-pointer'
           >
-            <FaChevronRight className='text-xl text-white' />
+            <VscChevronRight className='text-blue-600' size={40} />
           </button>
         </div>
       </div>
