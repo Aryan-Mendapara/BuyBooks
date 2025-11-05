63cb22ebd3666c71c579bd1b69a15f11b82d4a70
Aryan-Mendapara <aryanmendapara146@gmail.com>
2025-11-05 16:15:43 +0530

    New Arrivals

--- client/src/Component/Pages/NewArrivals/NewArrivals.jsx
+++ client/src/Component/Pages/NewArrivals/NewArrivals.jsx
@@ -9,7 +9,7 @@ import { addToBillingDetails } from '../../Redux/Slice/BillingDetailsSlice';
 import { ThemeContext } from '../../ThemeContext/ThemeContext';
 
 function NewArrivals() {
-    const {darkMode} = useContext(ThemeContext);
+    const { darkMode } = useContext(ThemeContext);
 
     const [isAnimating, setIsAnimating] = useState(false);
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -194,7 +194,11 @@ function NewArrivals() {
                                             <div className="relative mb-4 overflow-hidden px-4 pt-2">
                                                 <div className="relative pb-[133%]">
                                                     <img
-                                                        src={`${import.meta.env.VITE_BACKEND_URL}/${book.image}`}
+                                                        src={
+                                                            book.image.startsWith("http")
+                                                                ? book.image
+                                                                : `${import.meta.env.VITE_BACKEND_URL}/${book.image}`
+                                                        }
                                                         alt={book.title}
                                                         className="absolute inset-0 w-60 h-60 object-contain"
                                                     />
